9bf357d89fa42436e6a38cf0ce6d7ad4f8b55869
kahesr <60002689+kahesr@users.noreply.github.com>
2025-02-15 20:31:52 +0330

    Add pet-cards, using wp-query class to fetch posts data dynamically and display them on the page

--- build/awesome/render.php
+++ build/awesome/render.php
@@ -1,41 +1,61 @@
 <?php
 $ourArgumets = array(
     'post_type'      => 'pet', 
-    'posts_per_page' => -1, // Get all matching posts
-    'tax_query'      => array(
-        array(
-            'taxonomy' => 'species', // Your custom taxonomy
-            'field'    => 'slug',
-            'terms'    => 'dog',
-        ),
-    ),
-    'meta_query'     => array(
-        array(
-            'key'     => 'weight', // Custom field key
-            'value'   => 5,
-            'compare' => '>',
-            'type'    => 'NUMERIC',
-        ),
-    ),
+    // 'posts_per_page' => -1, // Get all matching posts
+    // 'tax_query'      => array(
+    //     array(
+    //         'taxonomy' => 'species', // Your custom taxonomy
+    //         'field'    => 'slug',
+    //         'terms'    => 'dog',
+    //     ),
+    // ),
+    // 'meta_query'     => array(
+    //     array(
+    //         'key'     => 'weight', // Custom field key
+    //         'value'   => 5,
+    //         'compare' => '>',
+    //         'type'    => 'NUMERIC',
+    //     ),
+    // ),
 );
 
  // Create the WP-Query object
-$bigDogsQuery = new WP_Query($ourArgumets); //wp-query is going to give up only the posts that we are looking for based on the argumet that we provided
+$petQuery = new WP_Query($ourArgumets); //wp-query is going to give up only the posts that we are looking for based on the argumet that we provided
 
 // Checks if any poste were returned
-if ($bigDogsQuery->have_posts()) {
-    while ($bigDogsQuery->have_posts()) {
-        $bigDogsQuery->the_post(); ?>
-		<div class="our-pet">
-			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+if ($petQuery->have_posts()) { ?>
+	<div class="pets-section alignfull"> <!-- in 2025 theme to have a container to take the whole width you can use "alignfull" class -->
+		<div class="pets-section-inner">
+			<h2>Our Pets</h2>
+			<div class="pet-cards-grid">
+					<?php while ($petQuery->have_posts()) {
+							$petQuery->the_post(); ?>
+								<div class="our-pet">
+									<div class="pet-photo">
+										<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+									</div>
+									<div class="pet-text">
+										<h3>
+											<a href="<?php the_permalink(); ?>">
+													<?php the_title(); ?> 
+													(<?php the_field('weight') ?> kg)
+											</a>
+										</h3>
+										<p><?php echo wp_trim_words(get_the_content(), 14) ?>
+											<a href="<?php the_permalink(); ?>" class="learn-more">learn more &raquo;</a>
+										</p>
+									</div>
+								</div>
+								
+								
+								<?php }
+				wp_reset_postdata();
+			} else {
+				echo 'No matching pets found.';
+			}
+			
+			?>
+			</div>
 		</div>
-
-
-		
-   <?php }
-    wp_reset_postdata();
-} else {
-    echo 'No matching pets found.';
-}
-
-?>
+	</div>
